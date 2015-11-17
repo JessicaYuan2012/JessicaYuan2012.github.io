8fc4e9051a8d9b789118eaf38422a78cf621fa1a
Yuan Yang <yuanyang3335@163.com>
2015-11-17 20:28:09 +0800

    first outlier

---
+++ spark_web_ui/js/index.js
@@ -0,0 +1,63 @@
+
+var lines = [];
+
+var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+var lineChartData = {
+	labels : ["January","February","March","April","May","June","July"],
+	datasets : [
+		{
+			label: "My First dataset",
+			fillColor : "rgba(220,220,220,0.2)",
+			strokeColor : "rgba(220,220,220,1)",
+			pointColor : "rgba(220,220,220,1)",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(220,220,220,1)",
+			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+		},
+		{
+			label: "My Second dataset",
+			fillColor : "rgba(151,187,205,0.2)",
+			strokeColor : "rgba(151,187,205,1)",
+			pointColor : "rgba(151,187,205,1)",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(151,187,205,1)",
+			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+		}
+	]
+}
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(data[j].replace(/\"/g, ""));
+            }
+            lines.push(tarr);
+        }
+    }
+    console.log(lines);
+}
+
+$(document).ready(function() {
+		//process csv data
+        $.ajax({
+            type: "GET",
+            url: "data/cars.csv",
+            dataType: "text",
+            success: function(data) {processData(data);}
+         });
+
+        //draw chart
+    	var ctx = document.getElementById("test-chart").getContext("2d");
+		window.myLine = new Chart(ctx).Line(lineChartData, {
+			responsive: true
+		});
+    });
