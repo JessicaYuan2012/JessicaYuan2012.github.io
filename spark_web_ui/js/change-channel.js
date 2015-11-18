b59077c31504869f71251940a02e3e43778c4b6d
Yuan Yang <yuanyang3335@163.com>
2015-11-18 10:19:29 +0800

    channel change daily

--- spark_web_ui/js/change-channel.js
+++ spark_web_ui/js/change-channel.js
@@ -38,7 +38,7 @@ var ChannelChangeAllOpts = {
       yAxisRight : false,
       xAxisBottom : true,
       xAxisTop : false,
-      yAxisLabel : "亿秒",
+      yAxisLabel : "用户数量",
             yAxisFontFamily : "'Arial'",
             yAxisFontSize : 16,
             yAxisFontStyle : "normal",
@@ -72,7 +72,83 @@ var ChannelChangeAllOpts = {
       startAngle : 0,
       dynamicDisplay : true,
       responsive : true
-}
+};
+
+var ChannelChangeDailyOpts = {
+      inGraphDataShow : true,
+      datasetFill : true,
+      scaleLabel: "<%=value%>",
+      scaleTickSizeRight : 5,
+      scaleTickSizeLeft : 5,
+      scaleTickSizeBottom : 5,
+      scaleTickSizeTop : 5,
+      scaleFontSize : 10,
+      /*canvasBorders : true,
+      canvasBordersWidth : 3,
+      canvasBordersColor : "black",*/
+      graphTitle : "日均换台次数折现图",
+            graphTitleFontFamily : "'Arial'",
+            graphTitleFontSize : 24,
+            graphTitleFontStyle : "normal",
+            graphTitleFontColor : "#666",
+      /*graphSubTitle : "Graph Sub Title",
+            graphSubTitleFontFamily : "'Arial'",
+            graphSubTitleFontSize : 18,
+            graphSubTitleFontStyle : "normal",
+            graphSubTitleFontColor : "#666",*/
+      /*footNote : "Footnote for the graph",
+            footNoteFontFamily : "'Arial'",
+            footNoteFontSize : 8,
+            footNoteFontStyle : "bold",
+            footNoteFontColor : "#666",*/
+      legend : false,
+        legendFontFamily : "'Arial'",
+        legendFontSize : 12,
+        legendFontStyle : "normal",
+        legendFontColor : "#666",
+      legendBlockSize : 15,
+      legendBorders : true,
+      legendBordersWidth : 1,
+      legendBordersColors : "#666",
+      yAxisLeft : true,
+      yAxisRight : false,
+      xAxisBottom : true,
+      xAxisTop : false,
+      yAxisLabel : "次",
+            yAxisFontFamily : "'Arial'",
+            yAxisFontSize : 16,
+            yAxisFontStyle : "normal",
+            yAxisFontColor : "#666",
+      /*xAxisLabel : "pX Axis Label",
+          xAxisFontFamily : "'Arial'",
+            xAxisFontSize : 16,
+            xAxisFontStyle : "normal",
+            xAxisFontColor : "#666",
+      yAxisUnit : "Y Unit",
+            yAxisUnitFontFamily : "'Arial'",
+            yAxisUnitFontSize : 8,
+            yAxisUnitFontStyle : "normal",
+            yAxisUnitFontColor : "#666",*/
+      annotateDisplay : false, 
+      spaceTop : 0,
+      spaceBottom : 0,
+      spaceLeft : 0,
+      spaceRight : 0,
+      logarithmic: false,
+//      showYAxisMin : false,
+      rotateLabels : "smart",
+      xAxisSpaceOver : 0,
+      xAxisSpaceUnder : 0,
+      xAxisLabelSpaceAfter : 0,
+      xAxisLabelSpaceBefore : 0,
+      legendBordersSpaceBefore : 0,
+      legendBordersSpaceAfter : 0,
+      footNoteSpaceBefore : 0,
+      footNoteSpaceAfter : 0, 
+      startAngle : 0,
+      dynamicDisplay : true,
+      responsive : true
+};
 
 function returnInt(element){
   return parseInt(element,10);
@@ -115,6 +191,7 @@ function loadChannelChangeData(){
       $(element.children()).remove();
   }
   loadChannelChangeAllData();
+  loadChannelChangeDailyData();
 }
 
 function loadChannelChangeAllData() {
@@ -167,6 +244,50 @@ function loadChannelChangeAllData() {
 
             var ctx = document.getElementById("chart4").getContext("2d");
             window.myLine = new Chart(ctx).Bar(barChartData, ChannelChangeAllOpts);
+        }
+     });
+}
+
+function loadChannelChangeDailyData() {
+    var element = $('#canvas-container');
+    var canvas5="<canvas id=\"chart5\"></canvas>";
+    element.append(canvas5);
+
+    var columns = [[]];
+
+    $.ajax({
+        type: "GET",
+        url: "data/ChangeChanneDaily/part-00000",
+        dataType: "text",
+        success: function(data) {
+            processData(data,columns);
+
+            date_list = columns[0];
+            change_sum_list = columns[1].map(returnInt);
+            device_num_list = columns[2].map(returnInt);
+            final_data_points = []
+            for (var i = 0; i <= columns[0].length - 1; i++) {
+              final_data_points.push(change_sum_list[i]/device_num_list[i]);
+            };
+            
+            var lineChartData = {
+                labels : date_list.slice(0,14),
+                datasets : [
+                    {
+                        label: "Channel Change Times - All",
+                        fillColor : "rgba(220,220,220,0.2)",
+                        strokeColor : "rgba(220,220,220,1)",
+                        pointColor : "rgba(220,220,220,1)",
+                        pointStrokeColor : "#fff",
+                        pointHighlightFill : "#fff",
+                        pointHighlightStroke : "rgba(220,220,220,1)",
+                        data : final_data_points.slice(0,14)
+                    }
+                ]
+            }
+
+            var ctx = document.getElementById("chart5").getContext("2d");
+            window.myLine = new Chart(ctx).Line(lineChartData, ChannelChangeDailyOpts);
         }
      });
 }
