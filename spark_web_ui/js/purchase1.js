c9180ad16fc89caeb3a5a4bf0b30b1980ff7bf8a
maggie <linym012@163.com>
2015-11-19 10:30:33 +0800

    no message

--- spark_web_ui/js/purchase1.js
+++ spark_web_ui/js/purchase1.js
@@ -1,3 +1,205 @@
+var allopts = {
+  //Boolean - If we show the scale above the chart data   -> Default value Changed
+  scaleOverlay : true,
+  //Boolean - If we want to override with a hard coded scale
+  scaleOverride : false,
+  //** Required if scaleOverride is true **
+  //Number - The number of steps in a hard coded scale
+  scaleSteps : null,
+  //Number - The value jump in the hard coded scale
+  scaleStepWidth : null,
+  //Number - The scale starting value
+  scaleStartValue : null,
+  //String - Colour of the scale line 
+  scaleLineColor : "rgba(0,0,0,.1)",
+  //Number - Pixel width of the scale line  
+  scaleLineWidth : 1,
+  //Boolean - Whether to show labels on the scale 
+  scaleShowLabels : true,
+  //Interpolated JS string - can access value
+  scaleLabel : "<%=value%>",
+  //String - Scale label font declaration for the scale label
+  scaleFontFamily : "'Arial'",
+  //Number - Scale label font size in pixels  
+  scaleFontSize : 12,
+  //String - Scale label font weight style  
+  scaleFontStyle : "normal",
+  //String - Scale label font colour  
+  scaleFontColor : "#666",  
+  ///Boolean - Whether grid lines are shown across the chart
+  scaleShowGridLines : true,
+  //String - Colour of the grid lines
+  scaleGridLineColor : "rgba(0,0,0,.05)",
+  //Number - Width of the grid lines
+  scaleGridLineWidth : 1, 
+  //Boolean - Whether the line is curved between points -> Default value Changed 
+  bezierCurve : false,
+  //Boolean - Whether to show a dot for each point -> Default value Changed
+  pointDot : false,
+  //Number - Radius of each point dot in pixels
+  pointDotRadius : 3,
+  //Number - Pixel width of point dot stroke
+  pointDotStrokeWidth : 1,
+  //Boolean - Whether to show a stroke for datasets
+  datasetStroke : true,
+  //Number - Pixel width of dataset stroke
+  datasetStrokeWidth : 2,
+  //Boolean - Whether to fill the dataset with a colour
+  datasetFill : true,
+  //Boolean - Whether to animate the chart             -> Default value changed
+  animation : false,
+  //Number - Number of animation steps
+  animationSteps : 60,
+  //String - Animation easing effect
+  animationEasing : "easeOutQuart",
+  //Function - Fires when the animation is complete
+  onAnimationComplete : null,
+  canvasBorders : true,
+  canvasBordersWidth : 30,
+  canvasBordersColor : "black",
+  yAxisLeft : true,
+  yAxisRight : true,
+  yAxisLabel : "Y axis",
+  yAxisFontFamily : "'Arial'",
+  yAxisFontSize : 50,
+  yAxisFontStyle : "normal",
+  yAxisFontColor : "#666",
+  xAxisLabel : "",
+  xAxisFontFamily : "'Arial'",
+  xAxisFontSize : 16,
+  xAxisFontStyle : "normal",
+  xAxisFontColor : "#666",
+  yAxisUnit : "UNIT",
+  yAxisUnitFontFamily : "'Arial'",
+  yAxisUnitFontSize : 12,
+  yAxisUnitFontStyle : "normal",
+  yAxisUnitFontColor : "#666",
+  graphTitle : "",
+  graphTitleFontFamily : "'Arial'",
+  graphTitleFontSize : 24,
+  graphTitleFontStyle : "bold",
+  graphTitleFontColor : "#666",
+  graphSubTitle : "",
+  graphSubTitleFontFamily : "'Arial'",
+  graphSubTitleFontSize : 18,
+  graphSubTitleFontStyle : "normal",
+  graphSubTitleFontColor : "#666",
+  footNote : "Footnote",
+  footNoteFontFamily : "'Arial'",
+  footNoteFontSize : 50,
+  footNoteFontStyle : "bold",
+  footNoteFontColor : "#666",
+  legend : true,
+  legendFontFamily : "'Arial'",
+  legendFontSize : 18,
+  legendFontStyle : "normal",
+  legendFontColor : "#666",
+  legendBlockSize : 30,
+  legendBorders : true,
+  legendBordersWidth : 30,
+  legendBordersColor : "#666",
+  //  ADDED PARAMETERS 
+  graphMin : "DEFAULT",
+  graphMax : "DEFAULT"
+  
+  }
+
+    var noopts = {
+  nooptions : "",
+  yAxisRight : true,
+  scaleTickSizeLeft : 0,  
+  scaleTickSizeRight : 0,  
+  scaleTickSizeBottom : 0,  
+  scaleTickSizeTop : 1
+
+
+  }
+
+    var onlyborderopts = {
+  canvasBorders : true,
+  canvasBordersWidth : 3,
+  canvasBordersColor : "black"
+  
+  }
+
+var nooptions = { }
+
+var newopts = {
+      inGraphDataShow : true,
+      datasetFill : true,
+      scaleLabel: "<%=value%>",
+      scaleTickSizeRight : 5,
+      scaleTickSizeLeft : 5,
+      scaleTickSizeBottom : 5,
+      scaleTickSizeTop : 5,
+      scaleFontSize : 16,
+      canvasBorders : true,
+      canvasBordersWidth : 3,
+      canvasBordersColor : "black",
+      graphTitle : "Graph Title",
+      graphTitleFontFamily : "'Arial'",
+      graphTitleFontSize : 24,
+      graphTitleFontStyle : "bold",
+      graphTitleFontColor : "#666",
+      graphSubTitle : "Graph Sub Title",
+      graphSubTitleFontFamily : "'Arial'",
+      graphSubTitleFontSize : 18,
+      graphSubTitleFontStyle : "normal",
+      graphSubTitleFontColor : "#666",
+      footNote : "Footnote for the graph",
+      footNoteFontFamily : "'Arial'",
+      footNoteFontSize : 8,
+      footNoteFontStyle : "bold",
+      footNoteFontColor : "#666",
+      legend : true,
+      legendFontFamily : "'Arial'",
+      legendFontSize : 12,
+      legendFontStyle : "normal",
+      legendFontColor : "#666",
+      legendBlockSize : 15,
+      legendBorders : true,
+      legendBordersWidth : 1,
+      legendBordersColors : "#666",
+      yAxisLeft : true,
+      yAxisRight : false,
+      xAxisBottom : true,
+      xAxisTop : false,
+      yAxisLabel : "Y Axis Label",
+      yAxisFontFamily : "'Arial'",
+      yAxisFontSize : 16,
+      yAxisFontStyle : "normal",
+      yAxisFontColor : "#666",
+      xAxisLabel : "pX Axis Label",
+      xAxisFontFamily : "'Arial'",
+      xAxisFontSize : 16,
+      xAxisFontStyle : "normal",
+      xAxisFontColor : "#666",
+      yAxisUnit : "Y Unit",
+      yAxisUnitFontFamily : "'Arial'",
+      yAxisUnitFontSize : 8,
+      yAxisUnitFontStyle : "normal",
+      yAxisUnitFontColor : "#666",
+      annotateDisplay : true, 
+      spaceTop : 0,
+      spaceBottom : 0,
+      spaceLeft : 0,
+      spaceRight : 0,
+      logarithmic: false,
+//      showYAxisMin : false,
+      rotateLabels : "smart",
+      xAxisSpaceOver : 0,
+      xAxisSpaceUnder : 0,
+      xAxisLabelSpaceAfter : 0,
+      xAxisLabelSpaceBefore : 0,
+      legendBordersSpaceBefore : 0,
+      legendBordersSpaceAfter : 0,
+      footNoteSpaceBefore : 0,
+      footNoteSpaceAfter : 0, 
+      startAngle : 0,
+      dynamicDisplay : true,
+      responsive : true
+}
+
 var SeasonPurchaseOpts = {
       inGraphDataShow : true,
       datasetFill : true,
@@ -277,7 +479,7 @@ function loadPurchaseData(){
   }
   loadSeasonPurchaseData();
   loadAvgPersonPurchase();
-  //loadPerDayViewingHour();
+  loadPurchaseSumAvgCost();
   
 }
 
@@ -340,7 +542,7 @@ function loadAvgPersonPurchase() {
 var label_list = [];
 var daily_viewing_hour_list = [];
 
-function loadPerDayViewingHour() {
+function loadPurchaseSumAvgCost() {
     var element = $('#canvas-container');
     var canvas2="<canvas id=\"chart2\"></canvas>"
     var nav = "<nav id=\"chart-nav\">\
@@ -355,12 +557,25 @@ function loadPerDayViewingHour() {
     var columns2 = [[]];
     $.ajax({
         type: "GET",
-        url: "data/ViewingTime/part-00000",
+        url: "data/Purchase/PurchaseSum&AvgCost.txt",
         dataType: "text",
         success: function(data) {
             processData(data,columns2);
-            label_list = columns2[0];
-            daily_viewing_hour_list = columns2[1].map(returnHour);
+            final_data_points1 = []
+            final_data_points1.push(columns2[0][0]).toFixed(3);
+            final_data_points1.push(columns2[0][1]).toFixed(3);
+            final_data_points1.push(columns2[0][2]).toFixed(3);
+            final_data_points1.push(columns2[0][3]).toFixed(3);
+            
+            console.log(final_data_points1)
+
+            final_data_points2 = []
+            final_data_points2.push(columns2[1][0]).toFixed(3);
+            final_data_points2.push(columns2[1][1]).toFixed(3);
+            final_data_points2.push(columns2[1][2]).toFixed(3);
+            final_data_points2.push(columns2[1][3]).toFixed(3);
+            console.log(final_data_points2)
+/*            
             var LineChartData = {
                 labels : columns2[0].slice(0,14),
                 datasets : [
@@ -376,11 +591,37 @@ function loadPerDayViewingHour() {
                     }
                 ]
             }
+*/
 
-            var ctx2 = document.getElementById("chart2").getContext("2d");
-            window.myLine = new Chart(ctx2).Line(LineChartData, perDayOnlineHourOpts);
+            var mydata1 = {
+              labels : ["2011年","2012年","2013年","2014年"],
+              datasets : [
+                {
+                  fillColor : "rgba(220,220,220,0.5)",
+                  strokeColor : "rgba(220,220,220,1)",
+                  pointColor : "rgba(220,220,220,1)",
+                  pointStrokeColor : "#fff",
+                  data : final_data_points1,
+                  title : "PurchaseSum"
+                },
+                {
+                  fillColor : "rgba(151,187,205,0.5)",
+                  strokeColor : "rgba(151,187,205,1)",
+                  pointColor : "rgba(151,187,205,1)",
+                  pointStrokeColor : "#fff",
+                  data : final_data_points2,
+                  title : "Average Cost"
+                }
+              ]
+            }
+            var ctx2 = document.getElementById("chart2").getContext("2d")
+            window.myLine = new Chart(ctx2).Bar(mydata1,newopts);
         }
      });
+
+
+
+
 }
 
 function loadSeasonPurchaseData() {
