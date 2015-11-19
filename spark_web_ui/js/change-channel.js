2b92eae39a9246fe1f58a767e1e9caa830301007
slf12 <slf12thuss@163.com>
2015-11-19 22:25:42 +0800

    最终版6

--- spark_web_ui/js/change-channel.js
+++ spark_web_ui/js/change-channel.js
@@ -231,7 +231,7 @@ function loadChannelChangeData(){
   contentContainer.attr("style","margin:20px;")
   contentContainer.addClass("col-sm-2")
   var comment = "<p>结论：<p><br>日均换台高峰出现在周六和周日</p>\
-                <br><br><br><br><br><br><br><br><br><br><br><br><br>\
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>\
                 <p>用户很爱换台</p>";
   contentContainer.append(comment);
 
